e47fd113eb860423faace96419b85db2696cdc09
felipe-segovia <fsegovia.vega@gmail.com>
2021-07-21 15:31:57 -0400

    Initial commit

---
+++ src/main/java/com/vs/isl/JavaMsIslApplication.java
@@ -0,0 +1,13 @@
+package com.vs.isl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMsIslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMsIslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vs/isl/JavaMsIslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vs.isl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMsIslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
